2d940f06c1e3272d56a5db5061e27d8cd695f27c
Shiv Bhatia <shivbhatia10@gmail.com>
2024-01-13 16:36:13 +0000

    Kind of fix the location permissions

--- App.tsx
+++ App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 import {
+  PermissionsAndroid,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -22,6 +23,7 @@ import MapView from 'react-native-maps';
 
 import { enableLatestRenderer } from 'react-native-maps';
 import { Icon, SearchBar } from '@rneui/themed';
+import { Button } from '@rneui/base';
 enableLatestRenderer();
 
 type SectionProps = PropsWithChildren<{
@@ -29,7 +31,6 @@ type SectionProps = PropsWithChildren<{
 }>;
 
 function SearchBarSection({ children, title }: SectionProps): React.JSX.Element {
-  // const isDarkMode = useColorScheme() === 'dark';
   const isDarkMode = false;
   const [searchBarText, setSearchBarText] = useState('')
   const updateSearch = (search: string) => {
@@ -64,40 +65,114 @@ function SearchBarSection({ children, title }: SectionProps): React.JSX.Element 
         searchIcon={<Icon name="search" type="font-awesome" color="#86939e" />}
         inputContainerStyle={{ backgroundColor: 'transparent', borderWidth: 0 }}
         containerStyle={{ backgroundColor: 'lightgrey', borderRadius: 10 }}
+        showLoading={searchBarText.length > 0}
       />
     </View >
   );
 }
 
+const requestLocationPermission = async () => {
+  console.log("Requesting location permission");
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'NextBus Location Permission',
+        message:
+          'NextBus needs access to your location ' +
+          'so you can find yourself on the map more easily.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the location');
+    } else {
+      console.log('Location permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
 function App(): React.JSX.Element {
-  // const isDarkMode = useColorScheme() === 'dark';
   const isDarkMode = false;
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const requestLocationPermission = async () => {
+    console.log("Requesting location permission");
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'NextBus Location Permission',
+          message:
+            'NextBus needs access to your location ' +
+            'so you can find yourself on the map more easily.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the location');
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+    checkLocationPermission();
+  };
+
+  const [hasLocationPermission, setHasLocationPermission] = useState(false);
+  const checkLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+      if (granted) {
+        console.log("Location permission granted");
+        setHasLocationPermission(true);
+      } else {
+        console.log("Location permission denied");
+        setHasLocationPermission(false);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    checkLocationPermission();
+  }, []);
+
   return (
     <SafeAreaView style={backgroundStyle}>
-      <View
-        style={{
-          backgroundColor: isDarkMode ? Colors.black : Colors.white,
-        }}>
-        <SearchBarSection title="Next Bus">
-          Where are you going?
-        </SearchBarSection>
-        <MapView
-          style={{ width: '100%', height: '95%' }}
-          initialRegion={{
-            latitude: 51.5014,
-            longitude: -0.1419,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.1,
-          }} />
-      </View>
-      <View>
-        <Text>Test</Text>
-      </View>
+      {hasLocationPermission ?
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <SearchBarSection title="Next Bus">
+            Where are you going?
+          </SearchBarSection>
+          <MapView
+            style={{ width: '100%', height: '95%' }}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            initialRegion={{
+              latitude: 51.5014,
+              longitude: -0.1419,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }} />
+        </View>
+        : <View>
+          <Button onPress={requestLocationPermission}>Request Location Permission</Button>
+        </View>}
     </SafeAreaView>
   );
 }
